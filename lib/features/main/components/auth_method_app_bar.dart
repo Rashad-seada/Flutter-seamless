701ba35b324d43dbd9007d1f07154e83892c7be6
Youssef8820 <youssef.ashour188@gmail.com>
2024-02-20 16:48:08 +0200

    chore: Added comment

---
+++ lib/features/main/components/auth_method_app_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AuthMethodAppBar extends StatelessWidget {
+  const AuthMethodAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack();
+  }
+}

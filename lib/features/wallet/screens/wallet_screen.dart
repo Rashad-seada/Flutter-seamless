eb8dead66ab71ddd3336a3b9aea1333172681064
Youssef8820 <youssef.ashour188@gmail.com>
2024-02-29 00:09:32 +0200

    chore: edited wallet screen

--- lib/features/wallet/screens/wallet_screen.dart
+++ lib/features/wallet/screens/wallet_screen.dart
@@ -2,7 +2,9 @@ import 'package:Mawthoq/core/config/app_theme.dart';
 import 'package:Mawthoq/core/views/widgets/main_button.dart';
 import 'package:Mawthoq/core/views/widgets/space.dart';
 import 'package:Mawthoq/features/account/views/components/account_page_app_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:sizer/sizer.dart';
 
 class WalletScreen extends StatelessWidget {
@@ -12,76 +14,92 @@ class WalletScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
+        padding: EdgeInsets.all(7.w),
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                AccountPageAppBar(
-                  label: 'Wallet',
-                ),
-                Text(
-                  'Balance',
-                  style: AppTheme.mainTextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 15.sp,
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'SAR',
-                      style: AppTheme.mainTextStyle(
-                        fontSize: 12.sp,
-                      ),
-                    ),
-                    Space(
-                      width: 1.w,
-                    ),
-                    Text(
-                      '1500',
-                      style: AppTheme.mainTextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15.sp,
-                      ),
-                    ),
-                  ],
-                ),
-                Space(
-                  height: 1.5.h,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    MainButton(
-                      borderRadius: BorderRadius.circular(1.5.w),
-                      height: 8.w,
-                      width: 40.w,
-                      label: Text(
-                        'ايداع',
-                        style: AppTheme.mainTextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                      color: AppTheme.secondary900,
-                    ),
-                    MainButton(
-                      borderRadius: BorderRadius.circular(1.5.w),
-                      height: 8.w,
-                      width: 40.w,
-                      label: Text(
-                        'سحب',
-                        style: AppTheme.mainTextStyle(
-                          color: AppTheme.secondary900,
-                        ),
-                      ),
-                      color: AppTheme.primary900,
-                    ),
-                  ],
-                ),
-              ],
+          AccountPageAppBar(
+            label: 'Wallet',
+          ),
+          Center(
+            child: Text(
+              'Balance',
+              style: AppTheme.mainTextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 15.sp,
+              ),
             ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'SAR',
+                style: AppTheme.mainTextStyle(
+                  fontSize: 12.sp,
+                ),
+              ),
+              Space(
+                width: 1.w,
+              ),
+              Text(
+                '1500',
+                style: AppTheme.mainTextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15.sp,
+                ),
+              ),
+            ],
+          ),
+          Space(
+            height: 1.5.h,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: MainButton(
+                  borderRadius: BorderRadius.circular(1.5.w),
+                  height: 8.w,
+                  label: Text(
+                    'Withdraw',
+                    style: AppTheme.mainTextStyle(
+                      color: AppTheme.secondary900,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  color: AppTheme.primary900,
+                ),
+              ),
+              Space(
+                width: 3.w,
+              ),
+              Expanded(
+                child: MainButton(
+                  borderRadius: BorderRadius.circular(1.5.w),
+                  height: 8.w,
+                  width: 40.w,
+                  label: Text(
+                    'Deposit',
+                    style: AppTheme.mainTextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  color: AppTheme.secondary900,
+                ),
+              ),
+            ],
+          ),
+          Space(
+            height: 3.h,
+          ),
+          Text(
+            'Transactions',
+            style: AppTheme.mainTextStyle(
+              color: AppTheme.secondary900,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Space(
+            height: 1.5.h,
           ),
         ],
       ),
